73c85e6335f401dc446493d9422a30f466e89964
Greg Orvik <gvorvik@gmail.com>
2018-08-06 19:16:56 -0500

    added register route
    added register route

---
+++ src/components/register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div >
+   {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'register your user'
+    }
+  }
+}
+</script>
