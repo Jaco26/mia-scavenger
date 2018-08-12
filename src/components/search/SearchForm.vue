b9563c94294672c816d928588b6e66831140f00b
Jacob Albright <jacob.albright23@gmail.com>
2018-08-12 12:35:40 -0500

    set up component files for a search form and search results

---
+++ src/components/search/SearchForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" placeholder="Search for art" v-model="searchPhrase">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchPhrase: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
